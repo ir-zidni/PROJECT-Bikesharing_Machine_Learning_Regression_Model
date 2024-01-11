5dadcc34f12a29b2b407098cc81202b5209d825d
Irfan Zidni <zidniirfan155@gmail.com>
2024-01-11 09:59:44 +0700

    Add files via upload

--- bikesharing_ml.py
+++ bikesharing_ml.py
@@ -64,10 +64,11 @@ def main():
 
     st.divider()
 
-    st.write('Weathersit Type:', '1: Clear, Few clouds, Partly cloudy, Partly cloudy', 
-             '2: Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, Mist',
-             '3: Light Snow, Light Rain + Thunderstorm + Scattered clouds, Light Rain + Scattered clouds',
-             '4: Heavy Rain + Ice Pallets + Thunderstorm + Mist, Snow + Fog')
+    st.write('Weathersit Type:') 
+    st.write('1: Clear, Few clouds, Partly cloudy, Partly cloudy')
+    st.write('2: Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, Mist')
+    st.write('3: Light Snow, Light Rain + Thunderstorm + Scattered clouds, Light Rain + Scattered clouds')
+    st.write('4: Heavy Rain + Ice Pallets + Thunderstorm + Mist, Snow + Fog')
     
     st.divider()
 
